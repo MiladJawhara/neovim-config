9fc0d0dec1c10b65735d2141217e4b2ba0bb0a23
Milad Jawhara <milad.jawhara@gmail.com>
2024-09-21 02:08:28 +0300

    add move line shortcuts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line up(n)" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line down(n)" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move line up(v)" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move line down(v)" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
