c770a2eb1550b763ae8d843b51a94fa67d497dcc
Milad Jawhara <milad.jawhara@veer.ae>
2024-10-03 13:07:51 +0400

    add dap mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,9 +1,9 @@
 require "nvchad.mappings"
 
+local dap = require("dap");
 -- add yours here
 
 local map = vim.keymap.set
-
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
@@ -12,5 +12,12 @@ map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line up(n)" })
 map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line down(n)" })
 map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move line up(v)" })
 map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move line down(v)" })
+
+
+map('n', '<F5>',dap.continue)
+map('n', '<F10>',dap.step_over)
+map('n', '<F11>',dap.step_into)
+map('n', '<F12>',dap.step_out)
+map('n', '<leader>b',dap.toggle_breakpoint)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,10 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.wrap = false;
+
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
